725771821b176ab7b1245ea2113efb2f2bbb571e
kinan <kinanbezem7@gmail.com>
2022-07-12 21:13:00 +0100

    manual_rps.py game bug fixed Scissor/Paper combination

--- manual_rps.py
+++ manual_rps.py
@@ -26,11 +26,30 @@ def get_winner(choice_dict):
         quit()
     return winner
 
+def get_winners(choice_dict):
+    # Decide who won
+    if "Rock" in choice_dict.values() and "Paper" in choice_dict.values():
+        winner = list(choice_dict.keys())[list(choice_dict.values()).index("Paper")]
+    elif "Rock" in choice_dict.values() and "Scissors" in choice_dict.values(): 
+        winner = list(choice_dict.keys())[list(choice_dict.values()).index("Rock")] 
+    elif "Paper" in choice_dict.values() and "Scissors" in choice_dict.values(): 
+        winner = list(choice_dict.keys())[list(choice_dict.values()).index("Scissors")]
+    elif choice_dict["User"]==choice_dict["Computer"]:
+        print("Draw!")
+        quit()
+    else:
+        print("Error please check letter case and try again")
+        quit()
+    return winner
+
+
+
+
 def play():
-    computer_choice = "Paper"   #get_computer_choice()
-    user_choice =   "Scissors"    # get_user_choice()
+    computer_choice = "Scissors"   #get_computer_choice()
+    user_choice =   "Paper"    # get_user_choice()
     choice_dict = {"User": user_choice, "Computer": computer_choice}
-    winner = get_winner(choice_dict)
+    winner = get_winners(choice_dict)
     print("The computer chose: ", computer_choice,"\n The user chose: ",user_choice)
     print("\n")
     print("The winner is: ", winner,"!")
