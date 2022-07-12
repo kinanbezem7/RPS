bc360c9e26afa7f52ea1929a35b4a28442c32a0a
kinan <kinanbezem7@gmail.com>
2022-07-12 20:09:35 +0100

    manual_rps.py game working

--- manual_rps.py
+++ manual_rps.py
@@ -9,10 +9,25 @@ def get_user_choice():
     user_choice = input("Enter Rock, Paper or Scissors: ")
     return user_choice
 
-
+def get_winner(choice_dict):
+    # Decide who won
+    if "Rock" and "Paper" in choice_dict.values():
+        winner = list(choice_dict.keys())[list(choice_dict.values()).index("Paper")]
+    elif "Rock" and "Scissors" in choice_dict.values(): 
+        winner = list(choice_dict.keys())[list(choice_dict.values()).index("Rock")]   
+    elif "Paper" and "Scissors" in choice_dict.values(): 
+        winner = list(choice_dict.keys())[list(choice_dict.values()).index("Scissors")]
+    elif choice_dict["User"]==choice_dict["Computer"]:
+        print("Draw!")
+    else:
+        print("Error please check letter case and try again")
+        quit()
+    return winner
 
 
 computer_choice = get_computer_choice()
 user_choice =  get_user_choice()
-
-print(computer_choice,user_choice)
+choice_dict = {"User": user_choice, "Computer": computer_choice}
+winner = get_winner(choice_dict)
+print("The computer chose: ", computer_choice,"\n The user chose: ",user_choice)
+print("The winner is ", winner,"!")
